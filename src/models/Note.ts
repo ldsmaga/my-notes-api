8c71ebc5c8263eef0da651b8d211ad1d3f224712
Leandro <ldzsmaga@gmail.com>
2024-01-17 13:21:46 -0300

    fix: remove user from note model

--- src/models/Note.ts
+++ src/models/Note.ts
@@ -4,13 +4,11 @@ import mongoose, { Document, Schema } from "mongoose";
 interface INote extends Document {
   title: string;
   content: string;
-  user: Schema.Types.ObjectId;
 }
 
 const noteSchema: Schema<INote> = new mongoose.Schema({
   title: { type: String, required: true },
   content: { type: String, required: true },
-  user: { type: Schema.Types.ObjectId, ref: "User", required: true },
 });
 
 const Note = mongoose.model<INote>("Note", noteSchema);
